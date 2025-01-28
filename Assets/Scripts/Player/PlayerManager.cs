4a8c5df0316baa870b0429f0467c7884e6487781
napenshe <mr.oshnokov@gmail.com>
2025-01-28 22:06:45 +0300

    MoHide Character finally implemented. There are some problems with Photon Sync yet

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -33,7 +33,7 @@ public class PlayerManager : MonoBehaviour
 	{
 		// Transform spawnpoint = SpawnManager.Instance.GetSpawnpoint();
 		// controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), spawnpoint.position, spawnpoint.rotation, 0, new object[] { PV.ViewID });
-        // controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), Vector3.zero, Quaternion.identity, 0, new object[] { PV.ViewID });
+        controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerWithCameraPrefab"), Vector3.zero, Quaternion.identity, 0, new object[] { PV.ViewID });
 	}
 
 	public void Die()
