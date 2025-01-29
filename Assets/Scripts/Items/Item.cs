879ec92e7ecadea7ef1718be5e816ca8e63c4870
napenshe <mr.oshnokov@gmail.com>
2025-01-29 09:24:06 +0300

    Guns visuals

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,7 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
     public ItemInfo itemInfo;
-    public GameObject gameObject;
+    public GameObject itemGameObject;
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,18 +2,19 @@ using Photon.Pun;
 using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
 using static System.Net.Mime.MediaTypeNames;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
 
-public class PlayerController: MonoBehaviourPunCallbacks
+public class PlayerController : MonoBehaviourPunCallbacks, IDamageable
 {
 
     [SerializeField] GameObject cameraHolder;
 
     [SerializeField] float mouseSensitivity, sprintSpeed, walkSpeed, jumpForce, smoothTime;
+
+    [SerializeField] Item[] items;
 
     int itemIndex;
     int previousItemIndex = -1;
@@ -44,6 +45,7 @@ public class PlayerController: MonoBehaviourPunCallbacks
     {
         if (PV.IsMine)
         {
+            EquipItem(0);
         }
         else
         {
@@ -61,7 +63,39 @@ public class PlayerController: MonoBehaviourPunCallbacks
         Move();
         Jump();
 
-        if (transform.position.y < -10f)
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (Input.GetKeyDown((i + 1).ToString()))
+            {
+                EquipItem(i);
+                break;
+            }
+        }
+
+        if (Input.GetAxisRaw("Mouse ScrollWheel") > 0f)
+        {
+            if (itemIndex >= items.Length - 1)
+            {
+                EquipItem(0);
+            }
+            else
+            {
+                EquipItem(itemIndex + 1);
+            }
+        }
+        else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0f)
+        {
+            if (itemIndex <= 0)
+            {
+                EquipItem(items.Length - 1);
+            }
+            else
+            {
+                EquipItem(itemIndex - 1);
+            }
+        }
+
+        if (transform.position.y < -20f)
         {
             Die();
         }
@@ -94,12 +128,34 @@ public class PlayerController: MonoBehaviourPunCallbacks
 
     void EquipItem(int _index)
     {
+        if (_index == previousItemIndex)
+            return;
 
+        itemIndex = _index;
+
+        items[itemIndex].itemGameObject.SetActive(true);
+
+        if (previousItemIndex != -1)
+        {
+            items[previousItemIndex].itemGameObject.SetActive(false);
+        }
+
+        previousItemIndex = itemIndex;
+
+        if (PV.IsMine)
+        {
+            Hashtable hash = new Hashtable();
+            hash.Add("itemIndex", itemIndex);
+            PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+        }
     }
 
     public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
     {
-        
+        if (changedProps.ContainsKey("itemIndex") && !PV.IsMine && targetPlayer == PV.Owner)
+        {
+            EquipItem((int)changedProps["itemIndex"]);
+        }
     }
 
     public void SetGroundedState(bool _grounded)
@@ -123,6 +179,8 @@ public class PlayerController: MonoBehaviourPunCallbacks
     [PunRPC]
     void RPC_TakeDamage(float damage, PhotonMessageInfo info)
     {
+        currentHealth -= damage;
+
         if (currentHealth <= 0)
         {
             Die();
